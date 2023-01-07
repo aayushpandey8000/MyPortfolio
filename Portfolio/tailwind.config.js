45b875a9342872fe81fe14c4673dad1e8679bae8
Aayush Pandey <97326217+aayushpandey8000@users.noreply.github.com>
2023-01-07 12:58:00 +0530

    Add files via upload

---
+++ Portfolio/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      screens:{
+        'xs':'340px',
+      },
+    },
+  },
+  plugins: [],
+}
